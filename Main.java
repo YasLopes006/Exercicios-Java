e4e170216dfe965788da66b40fc652a6e103ae90
YasLopes006 <yasmincloliveira19@gmail.com>
2025-05-13 20:04:24 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,3 +15,57 @@ public class Main {
         System.out.println("Velocidade final: " + carro.getVelocidade() + " km/h");
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("João", 25);
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade());
+
+        pessoa.setIdade(-5); 
+        pessoa.setNome("Maria");
+        pessoa.setIdade(30);
+
+        System.out.println("Novo nome: " + pessoa.getNome());
+        System.out.println("Nova idade: " + pessoa.getIdade());
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Produto produto = new Produto("Camiseta", 49.90);
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preço: R$ " + produto.getPreco());
+
+        produto.setPreco(-10); 
+        produto.setPreco(59.90); 
+
+        System.out.println("Novo preço: R$ " + produto.getPreco());
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Termometro t = new Termometro(25.0);
+        System.out.println("Temperatura atual: " + t.getTemperatura() + " °C");
+
+        t.setTemperatura(-300); 
+        t.setTemperatura(-100); 
+
+        System.out.println("Temperatura atualizada: " + t.getTemperatura() + " °C");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("Ana", 7.5);
+
+        System.out.println("Aluno: " + aluno.getNome());
+        System.out.println("Nota: " + aluno.getNota());
+        System.out.println("Aprovado? " + aluno.aprovado());
+
+        aluno.setNota(11); 
+        aluno.setNota(5.5); 
+        System.out.println("Nova nota: " + aluno.getNota());
+        System.out.println("Aprovado? " + aluno.aprovado());
+    }
+}
+
+
